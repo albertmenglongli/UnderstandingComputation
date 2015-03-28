212191d5cba684ce6674da61499e3504d4ae1efb
limenglong <albert.menglongli@gmail.com>
2015-03-29 00:44:24 +0800

    Code Format

--- Chapter2/small_step_semantic.rb
+++ Chapter2/small_step_semantic.rb
@@ -125,7 +125,7 @@ class Boolean<Struct.new(:value)
 
 end
 
-class LessThan<Struct.new(:left,:right)
+class LessThan<Struct.new(:left, :right)
   def to_s
     "#{left}<#{right}"
   end
@@ -140,9 +140,9 @@ class LessThan<Struct.new(:left,:right)
 
   def reduce(environment)
     if left.reducible?
-      LessThan.new(left.reduce(environment),right)
+      LessThan.new(left.reduce(environment), right)
     elsif right.reducible?
-      LessThan.new(left,right.reduce(environment))
+      LessThan.new(left, right.reduce(environment))
     else
       Boolean.new(left.value<right.value)
     end
@@ -167,10 +167,11 @@ class Variable<Struct.new(:name)
   end
 end
 
-class Machine<Struct.new(:expression,:environment)
+class Machine<Struct.new(:expression, :environment)
   def step
     self.expression=expression.reduce(environment)
   end
+
   public
   def run
     while expression.reducible?
@@ -185,55 +186,64 @@ class DoNothing
   def to_s
     'do-nothing'
   end
+
   def inspect
     "<<#{self}>>"
   end
+
   def ==(other_statement)
     other_statement.instance_of?(DoNothing)
   end
+
   def reducible?
     false
   end
 end
 
-class Assign <Struct.new(:name,:expression)
+class Assign <Struct.new(:name, :expression)
   def to_s
     "#{name}=#{expression}"
   end
+
   def inspect
     "<<#{self}>>"
   end
+
   def reducible?
     true
   end
+
   def reduce(environment)
     if expression.reducible?
-      [Assign.new(name,expression.reduce(environment)),environment]
+      [Assign.new(name, expression.reduce(environment)), environment]
     else
-      [DoNothing.new,environment.merge({name=>expression})]
+      [DoNothing.new, environment.merge({name => expression})]
     end
   end
 end
 
-class If<Struct.new(:condition,:consequence,:alternative)
+class If<Struct.new(:condition, :consequence, :alternative)
   def to_s
     "if (#{condition}) { #{consequence} } else { #{alternative} }"
   end
+
   def inspect
     "<<#{self}>>"
   end
+
   def reducible?
     true
   end
+
   def reduce(environment)
     if condition.reducible?
-      [If.new(condition.reduce(environment),consequence,alternative),environment]
+      [If.new(condition.reduce(environment), consequence, alternative), environment]
     else
       case condition
         when Boolean.new(true)
-          [consequence,environment]
+          [consequence, environment]
         when Boolean.new(false)
-          [alternative,environment]
+          [alternative, environment]
       end
     end
   end
@@ -243,29 +253,31 @@ res= Add.new(Multiply.new(Number.new(2), Number.new(3)), Multiply.new(Number.new
 
 puts Machine.new(res).run
 
-expression = LessThan.new(Minus.new(Number.new(9),Number.new(5)),Multiply.new(Number.new(2),Number.new(3)))
+expression = LessThan.new(Minus.new(Number.new(9), Number.new(5)), Multiply.new(Number.new(2), Number.new(3)))
 puts Machine.new(expression).run
 
-expressionWithVariables = LessThan.new(Variable.new(:x),Variable.new(:y))
-puts Machine.new(expressionWithVariables,{x:Number.new(7),y:Number.new(4)}).run
+expressionWithVariables = LessThan.new(Variable.new(:x), Variable.new(:y))
+puts Machine.new(expressionWithVariables, {x: Number.new(7), y: Number.new(4)}).run
 
 
 Object.send(:remove_const, :Machine)
-class Machine<Struct.new(:statement,:environment)
+class Machine<Struct.new(:statement, :environment)
   def step
-    self.statement,self.environment=statement.reduce(environment)
+    self.statement, self.environment=statement.reduce(environment)
   end
+
   def run
     while statement.reducible?
-      puts"#{statement},#{environment}"
+      puts "#{statement},#{environment}"
       step
     end
-    puts"#{statement},#{environment}"
+    puts "#{statement},#{environment}"
   end
 
 end
-assignStatement = Assign.new(:x,Add.new(Variable.new(:x),Number.new(3)))
-env={x:Number.new(5)}
-Machine.new(assignStatement,env).run
+assignStatement = Assign.new(:x, Add.new(Variable.new(:x), Number.new(3)))
+env={x: Number.new(5)}
+Machine.new(assignStatement, env).run
 
-Machine.new(If.new(LessThan.new(Number.new(2),Number.new(3)),Assign.new(Variable.new(:x),Number.new(2)),Assign.new(Variable.new(:x),Number.new(3))),{x:Number.new(1)}).run
+Machine.new(If.new(LessThan.new(Number.new(2), Number.new(3)), Assign.new(Variable.new(:x), Number.new(2)),
+                   Assign.new(Variable.new(:x), Number.new(3))), {x: Number.new(1)}).run
